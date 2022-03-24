aad09fbcc0175251e954d38ad85364da4827a841
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-24 18:00:42 +0100

    complete exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,15 @@ const km_to_travel = prompt("Quanti km devi percorrere?");
 // Chiedo l'età all'utente
 const user_age = prompt("Quanti anni hai?");
 // Calcolo il prezzo del biglietto in base ai km da percorrere
-const price_km_to_travel = km_to_travel * 0.21;
+const price = km_to_travel * 0.21;
 // Applico sconto del 20% SE minorenne e del 40% SE over 65
-const price_age_under_18 = price_km_to_travel * 20 / 100;
-const price_age_over_65 = price_km_to_travel * 40 / 100;
+const price_age_under_18 = price - price * 20 / 100;
+const price_age_over_65 = price - price * 40 / 100;
 // Comunico il prezzo del biglietto su schermo
+if (user_age < 18) {
+   document.getElementById("ticket_price").innerHTML = `Hai meno di 18 anni! Hai diritto ad uno sconto del 20%. Il prezzo del tuo biglietto è ${price_age_under_18.toFixed(2)} €` 
+} else if (user_age > 65) {
+    document.getElementById("ticket_price").innerHTML = `Hai più di 65 anni! Hai diritto ad uno sconto del 40%. Il prezzo del tuo biglietto è ${price_age_over_65.toFixed(2)} €`
+} else {
+    document.getElementById("ticket_price").innerHTML = `Il prezzo del tuo biglietto è ${price.toFixed(2)} €`
+}
