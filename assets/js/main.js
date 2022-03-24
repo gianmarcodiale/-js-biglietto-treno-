b6fa4cacf4b28ca642979d86e0decf0d0ff9aa93
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-03-24 15:41:22 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età. Sulla base delle informazioni ottenute dovrà calcolare il prezzo totale del viaggio secondo queste regole:
+- il prezzo del biglietto è definito in base ai chilometri (0,21€ al km )
+- applica uno sconto del 20% ai minorenni
+- applica uno sconto del 40% agli over 65 */
+
+// Chiedo il numero di chilometri all'utente
+const km_to_travel = prompt("Quanti km devi percorrere?")
+// Chiedo l'età all'utente
+// Calcolo il prezzo del biglietto in base ai km da percorrere
+// Applico sconto del 20% SE minorenne e del 40% SE over 65
+// Comunico il prezzo del biglietto su schermo
